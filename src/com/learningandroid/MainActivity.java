e667771aa897e4f43a01bbca5a00d8a243a3b868
bobsantosjr <bob.santosjr@gmail.com>
2014-08-13 04:31:14 +0800

    initial

---
+++ src/com/learningandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.learningandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button btnUIButton = (Button) findViewById(R.id.main_btn_ui_button);
+        
+        btnUIButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				startActivity(new Intent(getApplicationContext(), UIButtonActivity.class));
+			}
+		});
+    }
+}
